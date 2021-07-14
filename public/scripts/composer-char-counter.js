4bb44e039862c14d1779ae60127803704c2c9f02
vhuang5564 <vhuang5564@gmail.com>
2021-07-14 12:28:29 +0000

    added tweet counter, tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+  
+  const $text = $('#tweet-text');
+  $text.on('input', (event) => {
+    const value = $('#tweet-text').val().length; // length of character input
+    counterVal = $('.counter').val(140 - value); // changes output value of tweet
+
+    if (140 - value >= 0) { // changes number next to tweet if color is different
+      $('.counter').css('color', 'white')
+    } else if (140 - value < 0) {
+      $('.counter').css('color', 'red')
+    }
+
+  })
+})
