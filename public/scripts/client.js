7786cf6b1caecc164baebba9515b882ef434142e
vhuang5564 <vhuang5564@gmail.com>
2021-07-21 03:01:54 +0000

    move renderTweets and createTweet helper functions outside $(document).ready()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,34 @@
 /* global $ */
 
+const createTweet = (tweet) => {
+  const time = timeago.format(tweet.created_at);
+  const $avatars = $(`<img src="${tweet.user.avatars}">`);
+  const $name = $("<h6>").text(`${tweet.user.name}`);
+  const $handle = $(`<h6 style="margin-left: auto; padding: 2px">`).text(`${tweet.user.handle}`);
+  const $header = $('<header>').addClass('tweet-header');
+  $header.append($avatars, $name, $handle);
+
+  const $body = $(`<body>`).addClass('tweet-body').text(`${tweet.content.text}`);
+
+  const $time = $(`<h6>`).text(`${time}`);
+  const $icon = $(`<h6><i class="fas fa-flag"></i><i class="fas fa-retweet" style="margin: 0.2em"></i><i class="fas fa-heart">`).addClass('icons');
+  const $footer = $('<footer>').addClass('tweet-footer');
+  $footer.append($time, $icon);
+
+  const $article = $('<article>').addClass('tweet-container');
+  $article.append($header, $body, $footer);
+
+  return $article;
+};
+
+const renderTweets = function(tweets) { // empties out all tweets and remakes them
+  $('#tweet-container').empty();
+  for (const tweet of tweets) {
+    const $tweet = createTweet(tweet);
+    $('#tweet-container').prepend($tweet);
+  }
+};
+
 $(document).ready(() => {
   const fetchPosts = () => {
     $.getJSON('/tweets', (posts) => {
@@ -8,35 +37,6 @@ $(document).ready(() => {
   };
 
   fetchPosts();
-
-  const createTweet = (tweet) => {
-    const time = timeago.format(tweet.created_at);
-    const $avatars = $(`<img src="${tweet.user.avatars}">`);
-    const $name = $("<h6>").text(`${tweet.user.name}`);
-    const $handle = $(`<h6 style="margin-left: auto; padding: 2px">`).text(`${tweet.user.handle}`);
-    const $header = $('<header>').addClass('tweet-header');
-    $header.append($avatars, $name, $handle);
-
-    const $body = $(`<body>`).addClass('tweet-body').text(`${tweet.content.text}`);
-
-    const $time = $(`<h6>`).text(`${time}`);
-    const $icon = $(`<h6><i class="fas fa-flag"></i><i class="fas fa-retweet" style="margin: 0.2em"></i><i class="fas fa-heart">`).addClass('icons');
-    const $footer = $('<footer>').addClass('tweet-footer');
-    $footer.append($time, $icon);
-
-    const $article = $('<article>').addClass('tweet-container');
-    $article.append($header, $body, $footer);
-
-    return $article;
-  };
-
-  const renderTweets = function(tweets) { // empties out all tweets and remakes them
-    $('#tweet-container').empty();
-    for (const tweet of tweets) {
-      const $tweet = createTweet(tweet);
-      $('#tweet-container').prepend($tweet);
-    }
-  };
 
   const $form = $('#new-tweet');
   $form.on('submit', function(e) { // when text is input and tweet is pressed
@@ -53,5 +53,5 @@ $(document).ready(() => {
       $('.counter').val('140'); // resets counter after tweet
     });
   });
-
 });
+
